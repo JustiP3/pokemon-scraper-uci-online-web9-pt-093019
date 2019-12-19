4694bdf5794f16841568e2dfe9548d975c757ea2
Justin Phillips <justinphil02@yahoo.com>
2019-12-19 03:23:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
   def self.find(id, db)
     sql = "SELECT * FROM pokemon WHERE id = ?"
     db.execute(sql, id).map do |row|
-      Pokemon.new(row[0], row[1], row[2])
+      Pokemon.new(id: row[0], name: row[1], type: row[2])
     end.first
   end
 
